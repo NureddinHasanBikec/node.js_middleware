7a6067226c43bd2ae78a2b1161ad06a0575f9fa9
NureddinHasanBikec <nureddinhasanbikec@gmail.com>
2021-03-14 14:57:27 +0300

    Middleware

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require("express");
-require("dotenv").config()
+require("dotenv").config();
+const logger = require("morgan");
+
 
 const host = "localhost";
 const port = 5001 || process.env.port;
 const app = express();
+
+// Middleware
+
+app.use((req, res, next) => { // Authentication
+    console.log("Auth");
+    res.isAuth = true;
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("Controller // db");
+    if(res.isAuth) {
+        console.log("Private data")
+    } else {
+        console.log("not auth")
+    }
+    next();
+});
+
+app.use((req, res) => {
+    console.log("middleware 3");
+    if(true) {
+        res.send("send");
+    };
+});
 
 
 app.listen(port, host, () => {
