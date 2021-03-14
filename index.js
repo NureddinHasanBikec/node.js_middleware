a0b337303d2597aa6d2e38cf27a4ea5a475df4d6
NureddinHasanBikec <nureddinhasanbikec@gmail.com>
2021-03-14 16:22:37 +0300

    Middleware-css

--- index.js
+++ index.js
@@ -2,26 +2,22 @@ const express = require("express");
 require("dotenv").config();
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
+const path =require("path");
 
 
 const host = "localhost";
 const port = 5001 || process.env.port;
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 // Middleware
 
-app.use(cookieParser());
-
-app.use((req, res, next) => { 
-    console.log("Cookies: ", req.cookies);
-    next();
-});
-
-app.use((req, res) => {
-    console.log("middleware 2");
-    if(true) {
-        res.send("send");
-    };
+app.use(express.static(__dirname + "/public"))
+ 
+app.get( "/", (req, res) => {
+     res.render("index");
 });
 
 
